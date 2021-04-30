c422a632c5d4c0bada3607552497187387026af5
Perpetua-Kabute <kabuteperpetua@gmail.com>
2021-04-30 10:10:31 +0300

    Delete build

--- src/main/kotlin/com/perpetua/plugins/Routing.kt
+++ src/main/kotlin/com/perpetua/plugins/Routing.kt
@@ -1,22 +1,37 @@
 package com.perpetua.plugins
 
+import com.perpetua.entities.ToDo
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
 
 fun Application.configureRouting() {
+    install(CallLogging)
+    install(ContentNegotiation){
+        gson{
+            setPrettyPrinting()
+            disableHtmlEscaping()
+        }
+    }
     // Starting point for a Ktor app:
     routing {
 
-        val todo = listOf<>()
+        val todo = listOf<ToDo>(
+            ToDo(1, "Create a repository", true),
+            ToDo(2, "Start engaging with the content", true),
+            ToDo(3, "Finish the tutorial", false),
+            ToDo(4, "Do my own project now", false)
+        )
         get("/") {
             call.respondText("Hello World!")
         }
 //        route to all todos
         get("/todos"){
-
+            call.respond(todo)
         }
 //        route to a single to do
         get("/todos/{id}"){
